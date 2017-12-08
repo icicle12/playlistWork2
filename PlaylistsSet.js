dd81d234bb39fa82fbb0387e4adfe66b36c32b38
icicle12 <mistcomme@gmail.com>
2017-12-08 00:39:59 -0800

    Update PlaylistsSet.js

--- PlaylistsSet.js
+++ PlaylistsSet.js
@@ -19,7 +19,7 @@ registerPlugin({
     var message = ev.msg;
     var start = message.substring(0, 15); //'createplaylists '
     var start2 = message.substring(0, 8); //'createp '
-    if ((start == 'createplaylists ') || (start2 == 'createp '))
+    if ((start == 'createplaylist ') || (start2 == 'createp '))
         sinusbot.chatChannel('hi guys');
     });
 });
