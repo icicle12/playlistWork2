efd8954499dec5450ea0938a9878ce13fa22f6bc
icicle12 <mistcomme@gmail.com>
2017-12-08 00:24:08 -0800

    Update PlaylistsSet.js

--- PlaylistsSet.js
+++ PlaylistsSet.js
@@ -1,1 +1,11 @@
-
+// JavaScript source code
+registerPlugin({
+    name: 'Public Playlists',
+    version: '1.0',
+    backends: ['ts3', 'discord'],
+    engine: '>= 0.9.16',
+    description: 'Allows clients to create playlists',
+    author: 'Shawye H. <shawye@ucla.edu>',
+    vars: []
+}, function (sinusbot, config) {
+    sinusbot.on('chat', function (ev) {
