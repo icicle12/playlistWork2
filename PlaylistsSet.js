3366d7dc561a37bd2a7d2ac019dd1f4344d35d90
icicle12 <mistcomme@gmail.com>
2017-12-08 23:01:00 -0800

    Update PlaylistsSet.js

--- PlaylistsSet.js
+++ PlaylistsSet.js
@@ -1,15 +1,15 @@
 registerPlugin({
-name: 'Public Playlists',
-version: '1.0',
-backends: ['ts3', 'discord'],
-engine: '>= 0.9.16',
-description: 'Allows clients to create playlists',
-author: 'Shawye H. <shawye@ucla.edu> & Steven H. <stahill@ucsc.edu>',
-vars: []
+    name: 'Public Playlists',
+    version: '1.0',
+    backends: ['ts3', 'discord'],
+    engine: '>= 0.9.16',
+    description: 'Allows clients to create playlists',
+    author: 'Shawye H. <shawye@ucla.edu> & Steven H. <stahill@ucsc.edu>',
+    vars: []
 
 }, function (sinusbot, config) {
-sinusbot.on('chat', function (ev) {
-         if (ev.msg == '!playlists')
+    sinusbot.on('chat', function (ev) {
+        if (ev.msg == '!playlists')
         {
             var Omsg = 'Official Playlists: ';
             var Umsg = 'Unofficial Playlists: ';
@@ -22,7 +22,7 @@ sinusbot.on('chat', function (ev) {
             }
             var check = sinusbot.getVar('publicPlaylists');
             var playlists2 = [];
-            if ((typeof check != 'undefined') && (check.length != 0) {
+            if ((typeof check != 'undefined') && (check.length != 0)) {
                 playlists2.push.apply(playlists2, check);
             }
             else
@@ -37,17 +37,19 @@ sinusbot.on('chat', function (ev) {
             sinusbot.chatChannel(Omsg);
             sinusbot.chatChannel(Umsg);
         }
- if (ev.mode != 2)
-  return;
+        if (ev.mode != 2)
+            return;
 
-var check = sinusbot.getVar('publicPlaylists');
-var playlists = [];
-if (typeof check != 'undefined') {
-playlists.push.apply(playlists, check);
-}
+        var check = sinusbot.getVar('publicPlaylists');
+        var playlists = [];
+        if (typeof check != 'undefined') {
+            playlists.push.apply(playlists, check);
+        }
  
-var message = ev.msg;
-var start = message.substring(0, 16); //'createplaylists '
-var start2 = message.substring(0, 9); //'createp '
-if ((start == '!createplaylist ') || (start2 == '!createp '))
-//do stuff
+        var message = ev.msg;
+        var start = message.substring(0, 16); //'createplaylists '
+        var start2 = message.substring(0, 9); //'createp '
+        if ((start == '!createplaylist ') || (start2 == '!createp '))
+            sinusbot.chatChannel('placeholder');
+    });
+});
