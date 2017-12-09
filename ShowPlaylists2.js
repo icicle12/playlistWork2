8f9ee8595cfeb0d1be33d612aff03d86d4bae2d5
icicle12 <mistcomme@gmail.com>
2017-12-08 22:26:08 -0800

    Update and rename ShowPlaylists.js to ShowPlaylists2.js

---
+++ ShowPlaylists2.js
@@ -0,0 +1,12 @@
+registerPlugin({
+    name: 'ShowAllPlaylists',
+    version: '1.0',
+    description: 'Shows all playlists with !playlists',
+    author: 'Shawye H. <shawye@ucla.edu> & Steven H. <stahill@ucsc.edu>',
+	vars: []
+
+},	function (sinusbot, config)	{
+	sinusbot.on('chat', function(ev) {
+		
+	});
+});
