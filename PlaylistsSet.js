9065b0391c298745abad3285d5441c69ecfad003
icicle12 <mistcomme@gmail.com>
2017-12-08 22:58:46 -0800

    Update PlaylistsSet.js

--- PlaylistsSet.js
+++ PlaylistsSet.js
@@ -9,8 +9,36 @@ vars: []
 
 }, function (sinusbot, config) {
 sinusbot.on('chat', function (ev) {
-if (ev.mode != 2)
-return;
+         if (ev.msg == '!playlists')
+        {
+            var Omsg = 'Official Playlists: ';
+            var Umsg = 'Unofficial Playlists: ';
+            var playlists = sinusbot.playlists();
+            var len = playlists.length;
+            for (i = 0; i < len; i++) {
+                Omsg = Omsg + sinusbot.playlistGet(playlist[i]).name;
+                if (i + 1 != len)
+                    Omsg = Omsg + ', ';
+            }
+            var check = sinusbot.getVar('publicPlaylists');
+            var playlists2 = [];
+            if ((typeof check != 'undefined') && (check.length != 0) {
+                playlists2.push.apply(playlists2, check);
+            }
+            else
+                Umsg = 'No unofficial playlists';
+            var len2 = playlists2.length;
+            for (j = 0; j < len2; j++)
+            {
+                Umsg = Umsg + playlists[j];
+                if (j + 1 != len2)
+                    Umsg = Umsg + ', ';
+            }
+            sinusbot.chatChannel(Omsg);
+            sinusbot.chatChannel(Umsg);
+        }
+ if (ev.mode != 2)
+  return;
 
 var check = sinusbot.getVar('publicPlaylists');
 var playlists = [];
